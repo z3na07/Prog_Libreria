6be3dc1b9ad6807d37e6fc2f97b825a08167cb4c
z3na07 <150442545+z3na07@users.noreply.github.com>
2024-10-19 11:12:32 +0200

    Added: get method of Isbn and Inserisci Libro method
    Tested and functional

--- Libreria_Completo/src/Libro.java
+++ Libreria_Completo/src/Libro.java
@@ -1,8 +1,11 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-enum genere  {Romanzo, Manuale, enciclopedia, fumetto};
+enum genere {Romanzo, Manuale, enciclopedia, fumetto};
+
 public class Libro {
     //costruttore della classe
-    public Libro(String _isbn, String _editore, String _titolo, double _prezzo, boolean _venduto, String _autore ) {
+    public Libro(String _isbn, String _editore, String _titolo, double _prezzo, boolean _venduto, String _autore) {
         isbn = _isbn;
         editore = _editore;
         titolo = _titolo;
@@ -65,7 +68,11 @@ public class Libro {
         this.autore = autore;
     }
 
-//vari attributi
+    public String getIsbn() {
+        return isbn;
+    }
+
+    //vari attributi
     private String isbn;
 
 
@@ -81,12 +88,12 @@ public class Libro {
 
     //override del metodo to string in modo da farli stampare quello che vogliamo
     @Override
-    public String toString(){
+    public String toString() {
         return String.format("isbn libro: %s Editore libro: %s Titolo: %s Prezzo: %d Venduto %b\n", isbn, editore, titolo, prezzo, venduto);
     }
 
     //metodo ricerca di un libro in scaffale
-    private static void ricerca(Libro input, Libro [] scaffale, final int MAX_LIBRI){
+    private static void ricerca(Libro input, Libro[] scaffale, final int MAX_LIBRI) {
 
         String titolo;
         String autore;
@@ -94,14 +101,14 @@ public class Libro {
         System.out.println("Inserisci il titolo del libro che vuoi ricercare");
         titolo = input.titolo;
 
-        for(int i=0; i < MAX_LIBRI; i++){
+        for (int i = 0; i < MAX_LIBRI; i++) {
 
-            if(scaffale[i].titolo.equalsIgnoreCase(titolo)){
+            if (scaffale[i].titolo.equalsIgnoreCase(titolo)) {
 
                 System.out.println("Inserisci l'autore del tuo libro");
                 autore = input.autore;
 
-                if(scaffale[i].autore.equalsIgnoreCase(autore)){
+                if (scaffale[i].autore.equalsIgnoreCase(autore)) {
 
                     System.out.println("Il tuo libro è stato trovato, ecco i suoi dati");
                     System.out.println(scaffale[i].toString());
@@ -115,18 +122,57 @@ public class Libro {
         }
 
 
-
-
     }
 
 
+    public static Libro[] inserisciLibro(Libro[] scaffale, Scanner input) {
+
+        Libro libro = new Libro();
+        boolean errato = false;
+
+
+        System.out.println("**********************************");
+        System.out.println("INSERIMENTO LIBRO NELLO SCAFFALE");
+        System.out.println("**********************************");
+
+
+        System.out.println("Inserisci il titolo del libro");
+        libro.setTitolo(input.nextLine());
+
+        do {
+            System.out.println("Inserisci l'autore del libro");
+            libro.setAutore(input.nextLine());
+        } while (libro.autore.matches(".*\\d.*"));
 
 
 
+        while(true) {
+            try {
+                System.out.println("Inserisci il prezzo del Libro");
+                libro.setPrezzo(Double.parseDouble(input.next()));
+                break;
+            } catch (Exception e) {
+                System.out.println("Valore errato, reinserire");
+
+            }
+        }
+
+        input.nextLine();
+        do {
+            System.out.println("Inserisci ISBN del libro");
+            libro.setIsbn(input.nextLine());
+        } while (libro.getIsbn().length() != 12);
+
+
+        System.out.println("Inserisci l'editore del libro");
+        libro.setEditore(input.nextLine());
+
+
+        return scaffale;
+    }
 
 
 }
-
 
 
 
