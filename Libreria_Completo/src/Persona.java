6b79046ade8b4373110f62b4ea47b23356410bee
Hallo5685 <150442285+Hallo5685@users.noreply.github.com>
2024-10-23 12:19:46 +0200

    added methods of "accedi" and "registrati" for the login

--- Libreria_Completo/src/Persona.java
+++ Libreria_Completo/src/Persona.java
@@ -2,67 +2,23 @@ import java.util.Scanner;
 
 public class Persona
 {
-    public String getNome() {
-        return nome;
-    }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public boolean isPrestato() {
-        return prestato;
-    }
-
-    public void setPrestato(boolean prestato) {
-        this.prestato = prestato;
-    }
-
-    public String getTesseraBiblioteca() {
-        return tesseraBiblioteca;
-    }
-
-    public void setTesseraBiblioteca(String tesseraBiblioteca) {
-        this.tesseraBiblioteca = tesseraBiblioteca;
-    }
-    
-    public int getDurataDelPrestito() {
-        return durataDelPrestito;
-    }
-
-    public void setDurataDelPrestito(int durataDelPrestito) {
-        this.durataDelPrestito = durataDelPrestito;
-    }
-
-    public String getDataInizioPrestito() {
-        return dataInizioPrestito;
-    }
-
-    public void setDataInizioPrestito(String dataInizioPrestito) {
-        this.dataInizioPrestito = dataInizioPrestito;
-    }
-
-    public String getDataFinePrestito() {
-        return dataFinePrestito;
-    }
-
-    public void setDataFinePrestito(String dataFinePrestito) {
-        this.dataFinePrestito = dataFinePrestito;
-    }
 
     private String nome;
     private boolean prestato;
     private String tesseraBiblioteca;
-    
+
     //dovrà essere un valore costante
     private int durataDelPrestito;
     private String dataInizioPrestito;
     private String dataFinePrestito;
     //decidete se aggiungere prenota come attributo
+
+    private String password;
     private final int libriPrestatiMax;
 
     //creazione costruttore
-    public Persona(String _nome, boolean _prestato, String _tesseraBiblioteca, int _durataDelPrestito, String _dataInizioPrestito, String _dataFinePrestito, final int _libriPrestatiMax)
+    public Persona(String _nome, boolean _prestato, String _tesseraBiblioteca, int _durataDelPrestito, String _dataInizioPrestito, String _dataFinePrestito, final int _libriPrestatiMax, String _password)
     {
         this.nome = _nome;
         this.prestato = _prestato;
@@ -71,6 +27,7 @@ public class Persona
         this.dataInizioPrestito = _dataInizioPrestito;
         this.dataFinePrestito = _dataFinePrestito;
         this.libriPrestatiMax = _libriPrestatiMax;
+        this.password =_password;
     }
 
     public Persona()
@@ -85,6 +42,9 @@ public class Persona
 
     }
 
+    final int fMax = 50;
+    Persona [] catalogoPersone = new Persona[fMax];
+
 
     @Override
     public String toString()
@@ -98,6 +58,76 @@ public class Persona
 
     }*/
 
+    public String getNome()
+    {
+        return nome;
+    }
+
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+
+    public boolean isPrestato()
+    {
+        return prestato;
+    }
+
+    public void setPrestato(boolean prestato)
+    {
+        this.prestato = prestato;
+    }
+
+    public String getTesseraBiblioteca()
+    {
+        return tesseraBiblioteca;
+    }
+
+    public void setTesseraBiblioteca(String tesseraBiblioteca)
+    {
+        this.tesseraBiblioteca = tesseraBiblioteca;
+    }
+
+    public int getDurataDelPrestito()
+    {
+        return durataDelPrestito;
+    }
+
+    public void setDurataDelPrestito(int durataDelPrestito)
+    {
+        this.durataDelPrestito = durataDelPrestito;
+    }
+
+    public String getDataInizioPrestito()
+    {
+        return dataInizioPrestito;
+    }
+
+    public void setDataInizioPrestito(String dataInizioPrestito)
+    {
+        this.dataInizioPrestito = dataInizioPrestito;
+    }
+
+    public String getDataFinePrestito()
+    {
+        return dataFinePrestito;
+    }
+
+    public void setDataFinePrestito(String dataFinePrestito)
+    {
+        this.dataFinePrestito = dataFinePrestito;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
 
 
 
@@ -107,7 +137,7 @@ public class Persona
         //fai inserire da main la TesseraBiblioteca
         for(int i=0; i < catalogoPersone.length; i++)
         {
-            if(catalogoPersone[i].tesseraBiblioteca == utente1.tesseraBiblioteca)
+            if(catalogoPersone[i].tesseraBiblioteca.equalsIgnoreCase(utente1.tesseraBiblioteca))
             {
                 return "Bentoranto utente: "+ utente1.nome;
             }
@@ -131,7 +161,7 @@ public class Persona
             }
             else
             {
-                System.out.println("Il libro non è in prestito ");
+                System.out.println("Non ci sono libri presi in prestito al momento ");
             }
         }
 
@@ -146,10 +176,21 @@ public class Persona
     }
 
     //visualizza dati Utente
-    public static void visualizzaDatiUtente(Persona [] catalogoPersone, Persona utente)
+    public static void visualizzaDatiUtente(Persona [] catalogoPersone, String utenteDappoggio, Scanner scanner)
     {
         //Inserire Perona.tesseraBiblioteca
-        utente.toString();
+        System.out.println("Inserire tessera della biblioteca: ");
+        utenteDappoggio = scanner.nextLine();
+
+        for(int i=0; i < catalogoPersone.length; i++)
+        {
+            if(catalogoPersone[i].tesseraBiblioteca.equalsIgnoreCase(utenteDappoggio))
+            {
+                catalogoPersone[i].toString();
+            }
+
+        }
+
     }
 
     //metodo che la persona può utilizzare per vedere quanti libri ha in prestito al momento
@@ -161,6 +202,58 @@ public class Persona
     }
 
     //metodo inserimento persona in catalogoPersone
+    public static Persona [] registrati(Persona [] catalogoPersone, Scanner scanner, int contPersone) throws Exception
+    {
+        //crazione oggetto
+        Persona utente = new Persona();
+
+        //creazioni credenziali con controllo
+        do
+        {
+            System.out.println("Inserisci il tuo nome");
+            utente.setNome(scanner.next());
+        }while(utente.nome.matches(".*\\d.*"));
+
+        System.out.println("Inserisci la tua password");
+        utente.setPassword(scanner.next());
+
+        catalogoPersone[contPersone] = utente;
+
+        System.out.println("ACCOUNT CREATO \n\n");
+
+        //contPersone deve essere aumentato di uno nel main ogni volta che il petodo ha avuto successo
+
+        return catalogoPersone;
+    }
+
+
+    public static boolean accedi(Persona [] catalogoPersone, Scanner scanner)
+    {
+        String name,pass;
+
+        System.out.println("Inserisci l'id della tessera bibilioteca: ");
+        name = scanner.next();
+
+        System.out.println("Inserisci la password");
+        pass = scanner.next();
+
+        for(int i=0;i< catalogoPersone.length;i++)
+        {
+            if(catalogoPersone[i].nome.equals(name))
+            {
+                if(catalogoPersone[i].password.equals(pass))
+                {
+                    return true;
+                }
+
+            }
+
+        }
+
+        return false;
+    }
+
+
 
 
 
