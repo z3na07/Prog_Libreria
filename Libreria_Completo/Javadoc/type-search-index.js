31d254d4e880162c6b3c5982a94b39d44bb4e116
Hallo5685 <150442285+Hallo5685@users.noreply.github.com>
2024-11-13 12:25:12 +0100

    Documentazione

---
+++ Libreria_Completo/Javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Commesso"},{"p":"<Unnamed>","l":"Libro"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Persona"},{"p":"<Unnamed>","l":"Utility"}];updateSearchResults();
