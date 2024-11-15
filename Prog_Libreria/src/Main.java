ca58c3032927ee288e27db079f65c6b7bc29f395
Hallo5685 <150442285+Hallo5685@users.noreply.github.com>
2024-11-15 17:31:32 +0100

    Modifica del funzionamento dei vari menù
    Ho fatto in modo che dopo la registrazione di un cliente esso possa tornare al menù principale.
    Lo switch case che gestisce il commesso non è ancora in funzione ma è nel main

--- Prog_Libreria/src/Main.java
+++ Prog_Libreria/src/Main.java
@@ -1,67 +1,152 @@
 import java.util.Scanner;
-import java.time.LocalDate;
+//import java.time.LocalDate;
 import java.util.ArrayList;
+
+/**
+ * E' il punto di ingresso principale dell'applicazione.
+ * Contiene il metodo "main" che viene eseguito all'avvio del programma.
+ *
+ * @author Martino (Hallo5685), Leonardo (z3na07), Mattia (programmatoresperto)
+ * @version beta
+ */
 
 public class Main
 {
-    public static void main(String[] args)
-    {
+    /**
+     * Metodo principale dell'applicazione che viene eseguito ogni inzio programma
+     *
+     * @param args argomenti da riga di comando, che vengono passati all'applicazione al momento dell'esecuzione.
+     *
+     * @version beta
+     * */
+
+    public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        ArrayList <Persona> catalogoPersone = new ArrayList<>();
+        ArrayList<Persona> catalogoPersone = new ArrayList<>();
+        ArrayList<Libro> scaffale = new ArrayList<>();
 
-        String [] opzioniLogin = {"LOGIN ","entrata cliente","entrata commesso"};
-        String [] opzioni = {"BIBLIOTECA","Login","","","","USCITA"};
+        String[] opzioniLogin = {"MENU", "Registrati", "entrata cliente", "entrata commesso", "uscita"};
+        String[] opzioniCom = {"BIBLIOTECA", "Inserisci Libro", "Visualizza scaffale", "", "", "uscita"};
+        String[] opzioniUtente = {"MENU", "Acquista libro", "Cerca i tuoi libri", "Visualizza il catalogo", "Prendi in prestito", "uscita"};
 
+        boolean fineCommesso = true;
         boolean fineLogin = true;
         boolean fine = true;
 
         int contPersone = 0;
 
+        //creazione dei libri di defaul da metter di base nell'array list scaffale
+        Libro petrarca = new Libro("1234", "Enaudi", "Il Canzoniere ", 14, false, "Petrarca");
+        Libro boccaccio = new Libro("1738", "Giunti", "Il Decamerun", 15, false, "Boccaccio");
+        Libro dante = new Libro("1890", "Mondadori", "La Divina Commedia", 50, false, "Dante");
+        Libro leopardi = new Libro("900", "Feltrinelli", "L'infinito", 16, false, "Leopardi");
 
-        do
-        {
-            switch (Tools.Menu(opzioniLogin, keyboard))
+        scaffale.add(petrarca);
+        scaffale.add(boccaccio);
+        scaffale.add(dante);
+        scaffale.add(leopardi);
+
+        do {
+            switch (Utility.Menu(opzioniLogin, keyboard))
             {
                 case 1:
-                    //registrazione
+                    //registrazione utente
 
                     Persona nuovoUtente = new Persona();
-                    nuovoUtente = Persona.registrati(catalogoPersone, keyboard, contPersone);
+                    nuovoUtente = Persona.registrati(keyboard);
                     catalogoPersone.add(nuovoUtente);
 
-                    fineLogin = false;
+                    //ENTRATA NEL MENU DELL'UTENTE TRAMITE UNO SWITCH CASE
+                    //switch case che permetterà l'utilizzo el softwere dopo il login DEL CLIENTE
+                    do {
+                        switch (Utility.Menu(opzioniUtente, keyboard))
+                        {
+                            case 1:
+                                //acquista libro
+                                if (scaffale.isEmpty())
+                                {
+                                    System.out.println("Non ci sono libri da acquistare");
+                                } else
+                                {
+
+                                }
+                                break;
+
+                            case 2:
+                                break;
+
+                            case 3:
+                                //visualizza il catalogo
+                                Libro.visualizzaScaffale(scaffale);
+                                break;
+
+                            case 4:
+                                break;
+
+                            case 5:
+                                //esci dallo switch case
+                                fine = false;
+                                break;
+
+                        }
+                    } while (fine);
+
+                    //fineLogin = false;
                     break;
 
+                //CASE 2 DELLO SWITCH PRINCIPALE
                 case 2:
                     //accesso cliente
-                    if(Persona.accedi(catalogoPersone, keyboard) == true)
+                    if (Persona.accedi(catalogoPersone, keyboard))
                     {
                         System.out.println("Accesso eseguito corretamente \n");
+                        fineLogin = false;
+                    } else
+                    {
+                        System.out.println("Nome utente o password errata");
                     }
 
                     //esci dallo switch login per andare allo switch del menù
-                    fineLogin = false;
+
                     break;
 
                 case 3:
                     //accesso commesso
+                    Commesso commesso = new Commesso();
+
+                    System.out.println("Inserisci password");
+                    String passwordRapida = keyboard.nextLine();
+
+                    if(passwordRapida.equals(commesso.getPassword()))
+                    {
+
+                    }
+                    break;
+
+                case 4:
                     fineLogin = false;
+                    //uscita completa dal programma
+                    System.exit(0);
                     break;
             }
-        }while (fineLogin);
+        } while (fineLogin);
 
 
 
 
-        //switch case che permetterà l'utilizzo el softwere dopo il login
-        do
-        {
-            switch (Tools.Menu(opzioni, keyboard))
-            {
+        //switch case che permetterà l'utilizzo el softwere dopo il login DEL COMMESSO
+        do {
+            switch (Utility.Menu(opzioniCom, keyboard)) {
                 case 1:
+                    //inserimento libro
+                    Libro libroMomentaneo = new Libro();
+                    libroMomentaneo = Libro.inserisciLibro(keyboard);
+                    scaffale.add(libroMomentaneo);
+
                     break;
 
                 case 2:
+                    Libro.visualizzaScaffale(scaffale);
                     break;
 
                 case 3:
@@ -72,11 +157,10 @@ public class Main
 
                 case 6:
                     //esci dallo switch case
-                    fine = false;
+                    fineCommesso = false;
                     break;
             }
-        }while (fine);
-
+        } while (fineCommesso);
 
 
     }
