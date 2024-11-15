5af17d6fe8c8bfde31ae5644ff50755eccf16b94
Hallo5685 <150442285+Hallo5685@users.noreply.github.com>
2024-11-15 17:29:58 +0100

    Modifica metodo toString e visualizza

--- Prog_Libreria/src/Libro.java
+++ Prog_Libreria/src/Libro.java
@@ -1,16 +1,32 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 
 enum genere {romanzo, manuale, enciclopedia, fumetto};
+/**
+ * Classe che gestisce tutte le azioni sullo scaffale e sui libri stessi sia appena creati che quelli gia all'interno di scaffale
+ * @author Leonardo (z3na07)
+ * @version beta
+ * */
+public class Libro
+{
 
-public class Libro {
-
-    final int fMax = 50;
-    Libro[] scaffale = new Libro[fMax];
 
 
-    //costruttore della classe
+
+    /**
+     * Costruttore della classe {@code Libro}. Questo costruttore inizializza un oggetto {@code Libro}
+     * con i valori specificati per il codice ISBN, l'editore, il titolo, il prezzo, lo stato di vendita
+     * e l'autore.
+     *
+     * @param _isbn Il codice ISBN del libro. Un identificatore unico per ogni edizione del libro.
+     * @param _editore Il nome dell'editore del libro.
+     * @param _titolo Il titolo del libro.
+     * @param _prezzo Il prezzo del libro. Un valore numerico che rappresenta il costo del libro.
+     * @param _venduto Un valore booleano che indica se il libro è stato venduto (true) o meno (false).
+     * @param _autore Il nome dell'autore del libro.
+     */
     public Libro(String _isbn, String _editore, String _titolo, double _prezzo, boolean _venduto, String _autore) {
         isbn = _isbn;
         editore = _editore;
@@ -20,7 +36,9 @@ public class Libro {
         autore = _autore;
     }
 
-    //costruttore vuoto
+    /**
+     * Costruttore vuoto della classe {@code Libro}
+     */
     public Libro() {
         isbn = null;
         editore = null;
@@ -30,131 +48,196 @@ public class Libro {
         autore = null;
     }
 
-    //Tutti metodi get e set
+
+
+    /**
+     * Metodo set del attributo isbn
+     *
+     * @param isbn isbn del libro
+     * */
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
 
+    /**
+     * Metodo get del attributo Editore
+     *
+     * @return ritorna il valore del campo editore
+     */
     public String getEditore() {
         return this.editore;
     }
 
+    /**
+     * metodo set dell'attributo editore
+     *
+     * @param editore editore del libro
+     * */
     public void setEditore(String editore) {
         this.editore = editore;
     }
 
+    /**
+     * Metodo get dell'attributo titoloù
+     *
+     * @return ritorna il valore del campo titolo
+     * */
     public String getTitolo() {
         return this.titolo;
     }
 
+    /**
+     * Metodo set dell'attributo titolo
+     *
+     * @param titolo titolo del libro
+     * */
     public void setTitolo(String titolo) {
         this.titolo = titolo;
     }
 
+    /**
+     * Metodo get dell'attributo prezzo
+     *
+     * @return ritorna il valore del campo prezzo
+     * */
     public double getPrezzo() {
         return this.prezzo;
     }
 
+    /**
+     * Metodo set dell'attributo prezzo
+     *
+     * @param prezzo prezzo del libro
+     * */
     public void setPrezzo(double prezzo) {
         this.prezzo = prezzo;
     }
 
+    /**
+     * Metodo che ritorna se il libro è venduto oppure no
+     *
+     * @return ritorna il valore del campo venduto (true o false)
+     * */
     public boolean isVenduto() {
         return this.venduto;
     }
 
+    /**
+     * metodo set dell'attributo venduto
+     *
+     * @param venduto valore booleano che riconosce se il libro è stato venduto oppure no
+     * */
     public void setVenduto(boolean venduto) {
         this.venduto = venduto;
     }
 
+    /**
+     * Metodo get dell'attributo autore
+     *
+     * @return ritorna il valore del campo editore
+     * */
     public String getAutore() {
         return this.autore;
     }
 
+    /**
+     * Metodo set dell'attribvuto autore
+     *
+     * @param autore autore del libro
+     * */
     public void setAutore(String autore) {
         this.autore = autore;
     }
 
+    /**
+     * Metodo get dell'attributo isbn
+     *
+     * @return ritorna il valore del campo isbn
+     * */
     public String getIsbn() {
         return isbn;
     }
 
+    /**
+     * Metodo get dell'attributo tipo
+     *
+     * @return ritorna il calore del campo tipo
+     * */
     public genere getTipo() {
         return tipo;
     }
 
+    /**
+     * Metodo set dell'attributo tipo
+     *
+     * @param tipo attributo di tipo {@code enum} che ci dice se il libro è romanzo thriller o fumetto
+     * */
     public void setTipo(genere tipo) {
         this.tipo = tipo;
     }
 
     //vari attributi
     private String isbn;
-
-
     private String editore;
-
     private String titolo;
-
     private double prezzo;
-
     private boolean venduto;
-
     private String autore;
-
     private genere tipo;
 
 
-    //override del metodo to string in modo da farli stampare quello che vogliamo
+    /**
+     * Override del metodo toString
+     *
+     * @return riorna una {@code String} in cui sono stampati tutti gli attributi del libro e i loro corrispettivi valori
+     * */
     @Override
-    public String toString() {
-        return String.format("isbn libro: %s Editore libro: %s Titolo: %s Prezzo: %d Venduto %b\n", isbn, editore, titolo, prezzo, venduto);
+    public String toString()
+    {
+        return String.format("isbn libro: %s || Editore libro: %s || Titolo: %s || Prezzo: %.2f || Venduto: %b\n", isbn, editore, titolo, prezzo, venduto);
     }
 
-    //metodo ricerca di un libro in scaffale
-    private static void ricerca(Libro input, Libro[] scaffale, final int MAX_LIBRI) {
-
+    /**
+     * Metodo ricerca che ricera un libro all'interno dell'arraylist scaffale e ne stampa tutti i dati
+     *
+     * @param input scanner che legge i dati in input
+     * @param scaffale un {@code ArrayList} di tipo {@code Libro} al cui interno sono salvati tutti i libri
+     * */
+    private static void ricerca(Libro input, ArrayList<Libro> scaffale) {
         String titolo;
         String autore;
 
         System.out.println("Inserisci il titolo del libro che vuoi ricercare");
         titolo = input.titolo;
 
-        for (int i = 0; i < MAX_LIBRI; i++) {
-
-            if (scaffale[i].titolo.equalsIgnoreCase(titolo)) {
-
+        for (int i = 0; i < scaffale.size(); i++) {
+            if (scaffale.get(i).titolo.equalsIgnoreCase(titolo)) {
                 System.out.println("Inserisci l'autore del tuo libro");
                 autore = input.autore;
 
-                if (scaffale[i].autore.equalsIgnoreCase(autore)) {
-
+                if (scaffale.get(i).autore.equalsIgnoreCase(autore)) {
                     System.out.println("Il tuo libro è stato trovato, ecco i suoi dati");
-                    System.out.println(scaffale[i].toString());
-
-
+                    System.out.println(scaffale.get(i).toString());
                 }
-
-
             }
-
         }
-
-
     }
 
-    //metodo per inserimento del libro nello scaffale
-    public static Libro[] inserisciLibro(Libro[] scaffale, Scanner input, int contLibri)
+    /**
+     * Metodo inserisci libro che premette di inserire un libro all'interno dell'{@code ArrayLIst} Scaffale facendo inserire tutti gli attributi
+     *
+     * @param input Scanner che legge i dati in input
+     *
+     * @return il programma returna l'oggetto libro che poi verrà inserito in scaffale
+     * */
+    public static Libro inserisciLibro(Scanner input)
     {
-
         Libro libro = new Libro();
-        boolean errato = false;
-
         String[] tipoG = {"GENERE", "romanzo", "manuale", "enciclopedia", "fumetto"};
 
         System.out.println("**********************************");
         System.out.println("INSERIMENTO LIBRO NELLO SCAFFALE");
         System.out.println("**********************************");
-
 
         System.out.println("Inserisci il titolo del libro");
         libro.setTitolo(input.nextLine());
@@ -164,7 +247,6 @@ public class Libro {
             libro.setAutore(input.nextLine());
         } while (libro.autore.matches(".*\\d.*"));
 
-
         while (true) {
             try {
                 System.out.println("Inserisci il prezzo del Libro");
@@ -172,36 +254,42 @@ public class Libro {
                 break;
             } catch (Exception e) {
                 System.out.println("Valore errato, reinserire");
-
             }
         }
 
         input.nextLine();
-
         do {
             System.out.println("Inserisci ISBN del libro");
             libro.setIsbn(input.nextLine());
         } while (libro.getIsbn().length() != 12);
 
-
         System.out.println("Inserisci l'editore del libro");
         libro.setEditore(input.nextLine());
 
-
         System.out.println("Di che tipo è il tuo romanzo");
-        switch (Tools.Menu(tipoG, input)) {
-
+        switch (Utility.Menu(tipoG, input)) {
             case 1 -> libro.setTipo(genere.romanzo);
             case 2 -> libro.setTipo(genere.manuale);
             case 3 -> libro.setTipo(genere.enciclopedia);
             case 4 -> libro.setTipo(genere.fumetto);
-
         }
 
-        scaffale[contLibri] = libro;
-
-
-        return scaffale;
+        return libro;
     }
+
+    /**
+     * metodo che stampa a schermo tutti i dati di tutti i libri dell'{@code ArrayList} scaffale
+     *
+     * @param scaffale {@code ArrayList} di tipo {@code Libro} che contiene tutti i libri
+     * */
+    public static void visualizzaScaffale(ArrayList<Libro> scaffale)
+    {
+        for(Libro libro : scaffale)
+        {
+            System.out.println(libro);
+        }
+    }
+
+
 
 }
