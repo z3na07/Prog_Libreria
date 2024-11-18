a9f3de5b0cfab1b95d5e90585d59ebfc32d3cadd
Hallo5685 <150442285+Hallo5685@users.noreply.github.com>
2024-11-18 18:05:26 +0100

    Add implementazioni alle opzioni menù cliente

--- Libreria_Completo/src/Main.java
+++ Libreria_Completo/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 //import java.time.LocalDate;
 import java.util.ArrayList;
@@ -23,72 +24,143 @@ public class Main
     public static void main(String[] args)
     {
         Scanner keyboard = new Scanner(System.in);
+        Random random  = new Random();
+
         ArrayList<Persona> catalogoPersone = new ArrayList<>();
         ArrayList<Libro> scaffale = new ArrayList<>();
 
-        String[] opzioniLogin = {"MENU", "Registrati", "entrata cliente", "entrata commesso"};
-        String[] opzioniCom = {"BIBLIOTECA", "Inserisci Libro", "Visualizza scaffale", "", "", "USCITA"};
-        String[] opzioniUtente = {"MENU", "Acquista libro", "Cerca i tuoi libri", "Visualizza il catalogo", "Prendi in prestito"};
+        String[] opzioniLogin = {"MENU", "Registrati", "entrata cliente", "entrata commesso", "uscita"};
+        String[] opzioniCom = {"BIBLIOTECA", "Inserisci Libro", "Visualizza scaffale", "", "", "uscita"};
+        String[] opzioniUtente = {"MENU", "Acquista libro", "Cerca i tuoi libri", "Visualizza il catalogo", "Prendi in prestito","Visualizza dati utente", "uscita"};
 
+        //dichiarazioni boolean
         boolean fineCommesso = true;
         boolean fineLogin = true;
         boolean fine = true;
 
+        //dichiarazione variabili intere
         int contPersone = 0;
+        int indice = 0;
+        int numeroProgressivo = 0;
 
         //creazione dei libri di defaul da metter di base nell'array list scaffale
-        Libro petrarca = new Libro("1234", "Enaudi","Il Canzoniere ",120, false, "Petrarca");
-        Libro boccaccio = new Libro();
-        Libro dante = new Libro();
-        Libro leopardi = new Libro();
+        Libro petrarca = new Libro("1234", "Enaudi", "Il Canzoniere ", 14, false, "Petrarca");
+        Libro boccaccio = new Libro("1738", "Giunti", "Il Decamerun", 15, false, "Boccaccio");
+        Libro dante = new Libro("1890", "Mondadori", "La Divina Commedia", 50, false, "Dante");
+        Libro leopardi = new Libro("900", "Feltrinelli", "L'infinito", 16, false, "Leopardi");
+
+        scaffale.add(petrarca);
+        scaffale.add(boccaccio);
+        scaffale.add(dante);
+        scaffale.add(leopardi);
 
         do
         {
             switch (Utility.Menu(opzioniLogin, keyboard))
             {
                 case 1:
-                    //registrazione
+                    //registrazione utente
 
                     Persona nuovoUtente = new Persona();
                     nuovoUtente = Persona.registrati(keyboard);
                     catalogoPersone.add(nuovoUtente);
 
-                    fineLogin = false;
+                    //ENTRATA NEL MENU DELL'UTENTE TRAMITE UNO SWITCH CASE
+                    //switch case che permetterà l'utilizzo el softwere dopo il login DEL CLIENTE
+                    do {
+                        switch (Utility.Menu(opzioniUtente, keyboard))
+                        {
+                            case 1:
+                                //acquista libro
+                                if (scaffale.isEmpty())
+                                {
+                                    System.out.println("Non ci sono libri da acquistare");
+                                } else
+                                {
+                                    if(Libro.ricerca(scaffale, keyboard, indice))
+                                    {
+                                        System.out.println("Il prezzo da pagare è: "+scaffale.get(indice).getPrezzo()+" euro");
+                                        //System.out.println("Il libro verrà messo nel carrello ");
+                                    }
+                                    else
+                                    {
+                                        System.out.println("Il libro non è presente nel catalogo ");
+                                    }
+                                }
+                                break;
+
+                            case 2:
+                                break;
+
+                            case 3:
+                                //visualizza il catalogo
+                                Libro.visualizzaScaffale(scaffale);
+                                break;
+
+                            case 4:
+                                break;
+
+                            case 5:
+                                //visualizza dati dell'utente
+                                Persona.visualizzaDatiUtente(catalogoPersone, keyboard);
+
+                                break;
+
+                            case 6:
+
+                                //esci dallo switch case
+                                fine = false;
+                                break;
+                        }
+                    } while (fine);
+
                     break;
 
+                //CASE 2 DELLO SWITCH PRINCIPALE
                 case 2:
                     //accesso cliente
-                    if (Persona.accedi(catalogoPersone, keyboard)) {
+                    if (Persona.accedi(catalogoPersone, keyboard))
+                    {
                         System.out.println("Accesso eseguito corretamente \n");
-                    } else {
+                        fineLogin = false;
+                    } else
+                    {
                         System.out.println("Nome utente o password errata");
                     }
 
                     //esci dallo switch login per andare allo switch del menù
-                    fineLogin = false;
+
                     break;
 
                 case 3:
                     //accesso commesso
-                    System.out.println("Inserisci password");
+                    Commesso commesso = new Commesso();
 
+                    System.out.println("Inserisci password");
+                    String passwordRapida = keyboard.nextLine();
+
+                    if(passwordRapida.equals(commesso.getPassword()))
+                    {
+
+                    }
                     break;
 
-                default:
+                case 4:
                     fineLogin = false;
+                    //uscita completa dal programma
+                    System.exit(0);
                     break;
             }
         } while (fineLogin);
 
 
+
+
         //switch case che permetterà l'utilizzo el softwere dopo il login DEL COMMESSO
-        do
-        {
-            switch (Utility.Menu(opzioniCom, keyboard))
-            {
+        do {
+            switch (Utility.Menu(opzioniCom, keyboard)) {
                 case 1:
                     //inserimento libro
-
                     Libro libroMomentaneo = new Libro();
                     libroMomentaneo = Libro.inserisciLibro(keyboard);
                     scaffale.add(libroMomentaneo);
@@ -113,38 +185,29 @@ public class Main
         } while (fineCommesso);
 
 
-        //switch case che permetterà l'utilizzo el softwere dopo il login DEL CLIENTE
-        do
-        {
-            switch (Utility.Menu(opzioniUtente, keyboard))
-            {
-                case 1:
-                    if (scaffale.isEmpty())
-                    {
-                        System.out.println("Non ci sono libri da acquistare");
-                    } else
-                    {
+        /*
+        * import java.util.Random;
 
-                    }
-                    break;
-                case 2:
-                    //visualizza il catalogo
-                    Libro.visualizzaScaffale(scaffale);
-                    break;
+public class RandomStringGenerator {
+    public static String generateRandomString(int length) {
 
-                case 3:
-                    break;
+        //altre stringhe possibili: ABCDEFGHIJKLMNOPQRSTUVWXYZ
+        // Definisci l'insieme di caratteri
+        String characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuilder randomString = new StringBuilder();
 
-                case 4:
-                    break;
+        // Genera una stringa casuale
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            randomString.append(characters.charAt(index));
+        }
 
-                case 6:
-                    //esci dallo switch case
-                    fine = false;
-                    break;
+        return randomString.toString();
+    }
+}*/
 
-            }
 
-        } while (fine);
+
     }
 }
